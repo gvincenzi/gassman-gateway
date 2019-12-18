93bae4507ee2a630b6b9c4bca25adf4b01703abf
Giuseppe VINCENZI <gdvincenzi@airfrance.fr>
2019-12-19 00:41:40 +0100

    First commit

---
+++ src/main/java/org/gassman/gateway/GassmanGatewayApplication.java
@@ -0,0 +1,17 @@
+package org.gassman.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class GassmanGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GassmanGatewayApplication.class, args);
+    }
+
+}
